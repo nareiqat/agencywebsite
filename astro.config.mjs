2c6420c920e9df0612421b685fceb0de1876bf5b
Nabeel Areiqat <areiqat2@gmail.com>
2024-12-29 18:50:17 -0500

    fixed form submission

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
-import { defineConfig, envField } from "astro/config";
+import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
+import dotenv from "dotenv";
+dotenv.config();
+
 
 export default defineConfig({
   site: "https://www.yourwebsite.com", // update me!
---
+++ docs/_astro/hoisted.Dw9LoNQj.js
@@ -0,0 +1,1 @@
+import"./hoisted.CKc_fTbq.js";const c="6LesoqcqAAAAAK0YIWqWE65215WJfsaeZMFsclLE";console.log(c);grecaptcha.ready(function(){grecaptcha.execute(c,{action:"homepage"}).then(function(o){const e=document.getElementById("captchaResponse");e?e.value=o:console.error("Element with ID 'captchaResponse' not found.")})});
